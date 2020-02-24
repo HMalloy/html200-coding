c22e91f419dccff2f64f542a8b717573835a780f
Heather Malloy <hmalloy@bluetooth.com>
2020-02-24 15:40:31 -0800

    created new folder for lesson02 and added code to it

---
+++ Lesson02/calculator.js
@@ -0,0 +1,25 @@
+// https://codepen.io/HMalloy/pen/ZEGLoaz?editors=0011
+
+let x = 2;
+let y = 3;
+let z = 5;
+let a = 10;
+
+if ((x + y) === z ) {
+  console.log('Got it!');
+} else {
+  console.log('Nice try!');
+};
+
+while (x <= a) {
+  console.log('The song goes on til' + ' ' + a);
+  x++;
+};
+
+for (let x = 0; x < z; x++) {
+  console.log('Oooh, changed it up...');
+};
+
+if (((x + y) && (a - z)) <= 12) {
+  console.log('These equations are both less than 12!');
+};
