ba66c4bed76a0c43b061c9563c4963bfb7a42df3
Heather Malloy <hmalloy@bluetooth.com>
2020-03-03 16:49:06 -0800

    Lesson03-exercise-three added

---
+++ Lesson03/exercise-three.js
@@ -0,0 +1,10 @@
+// codepen: https://codepen.io/HMalloy/pen/MWwvqxX
+
+// Create an array that has at least 5 strings:
+// Use a for loop to console log all of the elements plus the index and a comma
+
+const family = ['Alex', 'Heather', 'Quinlan', 'Penelope', 'Beatrix'];
+
+for (let i = 0; i < family.length; i++) {
+    console.log(i + ", " + family[i]);
+}
